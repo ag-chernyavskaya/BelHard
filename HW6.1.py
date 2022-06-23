a860fc252bd1846953617ee6007fb76172057789
ag-chernyavskaya <a-hanevich@yandex.ru>
2022-06-23 19:19:55 +0300

    add something

--- HW6.1.py
+++ HW6.1.py
@@ -1,14 +1,20 @@
 right_name = 'angelina'
 right_password = 'ZXCV1234'
 
-def check(name, password, number = 3):
-    n = 1
-    while n < number:
+def check(name, password, number = 3, mess = 'Система заблоктрована. Повторите попытку через 5 минут.'):
+    if number < 0:
+        return mess
+    for i in range(number):
         if name == right_name and password == right_password:
-            return print('Вход выполнен!')
+            return 'Вход выполнен!'
         else:
-            name = input('Введите имя пользователя: ')
-            password = input('Введите пароль: ')
-            n += 1
-    print('Количество попыток для входа:', number,'Система заблокирована. Повторите попытку через 5 минут.')
-check(input('Введите имя пользователя: '), input('Введите пароль:' ))
+            name = input('Введите заново имя пользователя: ')
+            password = input('Введите заново пароль: ')
+            check(name=name, password=password,number=-1)
+
+def validate(input_login,input_password):
+    name = input_login
+    password = input_password
+    result = check(name=name,password=password)
+    return result
+print(validate('ang',123))
